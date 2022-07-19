ebcb769eafbf5b97c84e18cad6f22aee82b284f5
cocoder16 <cocoder16@gmail.com>
2022-07-19 15:22:06 +0900

    [Add] next/script

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Head from "next/head";
+import Script from "next/script";
 
 // Image 특징
 // 1. 빌드타임에 image를 최적화 하는 대신에 뷰포트에서 lazy load할 떄 최적화한다.
@@ -11,12 +12,24 @@ import Head from "next/head";
 // Head 특징
 // 해당 페이지에 대해서 head 태그안에 내용을 수정할 수 있다.
 
+// Script 특징
+// 추가적인 script를 fetch하고 실행할 때, 최적화한다.
+// login 페이지에서 추가 스크립트를 lazy load로 불러오도록 아래 코드를 짰다.
+// 불러온 script는 head 태그 안에 붙는다. 이후 페이지를 변경해도 script가 남아있는 것이 확인되었다.
+
 function Login() {
   return (
     <div>
       <Head>
         <title>Next App - Login</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h2>login</h2>
       <Image
         src="/images/profile.jpg" // Route of the image file
