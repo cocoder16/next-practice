c41bc6cb49a99835b8bccd7d42341854f2aa86bf
cocoder16 <cocoder16@gmail.com>
2022-07-19 14:19:02 +0900

    [Add] layout, route

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+// routing library가 따로 필요없다.
+// Link는 client side navigation
+// Link가 브라우저 뷰포트에 있으면, Next는 그 링크와 연결된 페이지를 background에서 prefetch하여 split code를 받아온다.
+
+function Header() {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <Link href="/login">Login</Link>
+    </div>
+  );
+}
+
+export default Header;
---
+++ components/Layout.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+}
+
+export default Layout;
